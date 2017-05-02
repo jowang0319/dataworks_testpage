cc425923e7d0ee61e3b620602df8a250163746ec
zhizhou wang <zhizhouwang@zhizhous-iMac.local>
2017-05-01 20:24:46 -0400

    update css and images

---
+++ js/map.js
@@ -0,0 +1,23 @@
+
+
+var mymap = L.map('location_map')
+	.setView([39.9849005,116.305622], 15); 
+
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	maxZoom: 17
+}).addTo(mymap);
+
+
+var greyIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.marker([39.9849005,116.305622], {icon: greyIcon}).addTo(mymap);
+
